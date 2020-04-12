ca65629a51fae50534bae942f7c0fb4240deb792
coffeebi <merfais.bwq@163.com>
2020-04-12 14:51:52 +0800

    增加设置基金发行（可买入）日期的函数

---
+++ src/setStartAt.js
@@ -0,0 +1,25 @@
+const _ = require('lodash')
+const db = require('./utils/db.js')
+
+function calc2() {
+  const startAt = `
+    update fund_info t1,
+      (SELECT code, min(date) date from fund_daily_state GROUP BY code) t2
+    set t1.start_at = t2.date
+    WHERE t1.code = t2.code
+  `
+  return db.query(startAt, 'fund_info', 'update')
+}
+
+function run() {
+  return calc2().then(r => {
+    console.log(r)
+    db.end()
+  }).catch(err => {
+    console.log(err)
+    db.end()
+  })
+}
+
+run()
+
